c4b552d5d0329bbf1d02b7ad9c8b1a400b43c59d
kylewong975 <kyle.wong975@gmail.com>
2017-04-01 23:19:15 -0700

    Load JS scripts with manifest

--- src/js/append-list.js
+++ src/js/append-list.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-	
+	$('.list-button').click(function(){
+		var tmp = $('input[name=list-item]').val();
+		//TODO Parse string to handle http(s) and edge cases
+    	$('.list-area').append('<li>' + tmp + '</li>');
+	});
 });
