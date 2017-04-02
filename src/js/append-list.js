7d80b106b6b90685bf9e85a6e82803ed737ea73d
kylewong975 <kyle.wong975@gmail.com>
2017-04-02 00:18:36 -0700

    Add list append functionality

--- src/js/append-list.js
+++ src/js/append-list.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
 	$('.list-button').click(function(){
 		var tmp = $('input[name=list-item]').val();
 		//TODO Parse string to handle http(s) and edge cases
-    	$('.list-area').append('<li>' + tmp + '</li>');
+    	$('.list-area').append('<li class="list-element">' + tmp + '<div class="close-button"><b>X</b></div>' + '</li>');
 	});
 });
