991ad648efb84eeaba235f721f3bf73d1d30bf1d
kylewong975 <kyle.wong975@gmail.com>
2017-04-02 00:46:42 -0700

    Add list remove functionality

--- src/js/append-list.js
+++ src/js/append-list.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
-	$('.list-button').click(function(){
+	var counter = 0;
+
+	$('.list-button').on('click', manager);
+
+	function manager(){
 		var tmp = $('input[name=list-item]').val();
 		//TODO Parse string to handle http(s) and edge cases
-    	$('.list-area').append('<li class="list-element">' + tmp + '<div class="close-button"><b>X</b></div>' + '</li>');
-	});
+		$('.list-area').append('<li class="list-element">' + tmp + '<div class="close-button" id="close' + counter + '"><b>X</b></div>' + '</li>');
+		$('#close' + counter).click(function(){
+			$(this).parent().remove();
+		});
+
+		counter++;
+	};
 });
